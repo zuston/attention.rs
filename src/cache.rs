6c682820eae1ed3f7fc228c48dfacb620a28cb6c
Guoqing Bao <topon@outlook.com>
2025-11-10 10:43:03 +0000

    Support FP8 kvcache offloading

--- src/cache.rs
+++ src/cache.rs
@@ -7,127 +7,159 @@ pub fn swap_blocks(
     dst: &Tensor,
     block_mapping: &HashMap<usize, usize>,
 ) -> Result<()> {
+    use candle_core as candle;
     use candle_core::cuda_backend::cudarc::driver::{result, CudaSlice, DevicePtr};
+    use candle_core::DType;
+    use half::{bf16, f16};
     use std::slice;
-    let block_size_elements = src.elem_count() / src.dim(0)?;
-    let (src_storage, _) = src.storage_and_layout();
-    let (dst_storage, _) = dst.storage_and_layout();
+    fn cuda_fwd<
+        T: candle::cuda_backend::CudaDType
+            + candle::cuda_backend::cudarc::driver::DeviceRepr
+            + candle_core::WithDType,
+    >(
+        src: &Tensor,
+        dst: &Tensor,
+        block_mapping: &HashMap<usize, usize>,
+    ) -> Result<()> {
+        let block_size_elements = src.elem_count() / src.dim(0)?;
+        let (src_storage, _) = src.storage_and_layout();
+        let (dst_storage, _) = dst.storage_and_layout();
+        let dtype_size = src.dtype().size_in_bytes();
 
-    match (src.device(), dst.device()) {
-        (Device::Cpu, Device::Cuda(dst_dev)) => {
-            let Storage::Cpu(src_storage) = &*src_storage else {
-                candle_core::bail!("Invalid source kvcache storage!")
-            };
-            let Storage::Cuda(dst_storage) = &*dst_storage else {
-                candle_core::bail!("Invalid dst kvcache storage!")
-            };
-            let cpu_num_blocks = src.dim(0)?;
-            let gpu_num_blocks = dst.dim(0)?;
-
-            let dst_ptr = dst_storage.as_cuda_slice::<half::bf16>()?.device_ptr();
-            let src_slice: &[half::bf16] = src_storage.as_slice()?;
-
-            for (src_block_number, dst_block_number) in block_mapping {
-                let src_offset: usize = src_block_number * block_size_elements;
-                assert!(
-                    *src_block_number < cpu_num_blocks,
-                    "Invalid cpu block {} / {}",
-                    src_block_number,
-                    cpu_num_blocks
-                );
-                assert!(
-                    *dst_block_number < gpu_num_blocks,
-                    "Invalid gpu block {} / {}",
-                    dst_block_number,
-                    gpu_num_blocks
-                );
-
-                assert!(
-                    src_offset + block_size_elements <= src_slice.len(),
-                    "Invalid cpu kvcache block {} for offload",
-                    src_block_number
-                );
-
-                let dst_offset: u64 = (dst_block_number * block_size_elements).try_into().unwrap();
-                let dst_slice: std::mem::ManuallyDrop<CudaSlice<half::bf16>> = unsafe {
-                    let slice =
-                        dst_dev.upgrade_device_ptr(dst_ptr + dst_offset, block_size_elements);
-                    std::mem::ManuallyDrop::new(slice)
+        match (src.device(), dst.device()) {
+            (Device::Cpu, Device::Cuda(dst_dev)) => {
+                let Storage::Cpu(src_storage) = &*src_storage else {
+                    candle_core::bail!("Invalid source kvcache storage!")
                 };
+                let Storage::Cuda(dst_storage) = &*dst_storage else {
+                    candle_core::bail!("Invalid dst kvcache storage!")
+                };
+                let cpu_num_blocks = src.dim(0)?;
+                let gpu_num_blocks = dst.dim(0)?;
 
-                unsafe {
-                    result::memcpy_htod_async(
-                        *dst_slice.device_ptr(),
-                        &src_slice[src_offset..src_offset + block_size_elements],
-                        *dst_dev.cu_stream(),
-                    )
+                let dst_ptr = *dst_storage.as_cuda_slice::<T>()?.device_ptr();
+                let src_slice: &[T] = src_storage.as_slice()?;
+
+                for (src_block_number, dst_block_number) in block_mapping {
+                    let src_offset: usize = src_block_number * block_size_elements;
+                    assert!(
+                        *src_block_number < cpu_num_blocks,
+                        "Invalid cpu block {} / {}",
+                        src_block_number,
+                        cpu_num_blocks
+                    );
+                    assert!(
+                        *dst_block_number < gpu_num_blocks,
+                        "Invalid gpu block {} / {}",
+                        dst_block_number,
+                        gpu_num_blocks
+                    );
+
+                    assert!(
+                        src_offset + block_size_elements <= src_slice.len(),
+                        "Invalid cpu kvcache block {} for offload",
+                        src_block_number
+                    );
+
+                    let dst_offset: u64 = (dst_block_number * block_size_elements * dtype_size)
+                        .try_into()
+                        .unwrap();
+                    let dst_slice: std::mem::ManuallyDrop<CudaSlice<T>> = unsafe {
+                        let slice = dst_dev.upgrade_device_ptr(
+                            dst_ptr.wrapping_add(dst_offset),
+                            block_size_elements * dtype_size,
+                        );
+                        std::mem::ManuallyDrop::new(slice)
+                    };
+
+                    unsafe {
+                        result::memcpy_htod_async(
+                            *dst_slice.device_ptr(),
+                            &src_slice[src_offset..src_offset + block_size_elements],
+                            *dst_dev.cu_stream(),
+                        )
+                        .map_err(candle_core::Error::wrap)?
+                    }
+                }
+                dst_dev.synchronize()
+            }
+            (Device::Cuda(src_dev), Device::Cpu) => {
+                let Storage::Cuda(src_storage) = &*src_storage else {
+                    candle_core::bail!("Invalid source kvcache storage!")
+                };
+                let Storage::Cpu(dst_storage) = &*dst_storage else {
+                    candle_core::bail!("Invalid dst kvcache storage!")
+                };
+                let gpu_num_blocks = src.dim(0)?;
+                let cpu_num_blocks = dst.dim(0)?;
+
+                let src_ptr = src_storage
+                    .as_cuda_slice::<T>()
                     .map_err(candle_core::Error::wrap)?
+                    .device_ptr();
+                let dst_slice: &[T] = dst_storage.as_slice().map_err(candle_core::Error::wrap)?;
+                let ptr = dst_slice.as_ptr() as *mut u8;
+
+                for (src_block_number, dst_block_number) in block_mapping {
+                    assert!(
+                        *src_block_number < gpu_num_blocks,
+                        "Invalid gpu block {} / {}",
+                        src_block_number,
+                        gpu_num_blocks
+                    );
+                    assert!(
+                        *dst_block_number < cpu_num_blocks,
+                        "Invalid cpu block {} / {}",
+                        dst_block_number,
+                        cpu_num_blocks
+                    );
+
+                    let src_offset: u64 = (src_block_number * block_size_elements * dtype_size)
+                        .try_into()
+                        .unwrap();
+                    let dst_offset: usize = (dst_block_number * block_size_elements * dtype_size)
+                        .try_into()
+                        .unwrap();
+                    let dst_slice = unsafe {
+                        slice::from_raw_parts_mut(
+                            ptr.wrapping_add(dst_offset),
+                            block_size_elements * dtype_size,
+                        )
+                    };
+
+                    let src_slice = unsafe {
+                        let slice: CudaSlice<T> = src_dev.upgrade_device_ptr(
+                            src_ptr.wrapping_add(src_offset),
+                            block_size_elements * dtype_size,
+                        );
+                        std::mem::ManuallyDrop::new(slice)
+                    };
+
+                    unsafe {
+                        result::memcpy_dtoh_async(
+                            dst_slice,
+                            *src_slice.device_ptr(),
+                            *src_dev.cu_stream(),
+                        )
+                        .map_err(candle_core::Error::wrap)?;
+                    }
                 }
+                src_dev.synchronize()
             }
-            let _ = dst_dev.synchronize();
-        }
-        (Device::Cuda(src_dev), Device::Cpu) => {
-            let Storage::Cuda(src_storage) = &*src_storage else {
-                candle_core::bail!("Invalid source kvcache storage!")
-            };
-            let Storage::Cpu(dst_storage) = &*dst_storage else {
-                candle_core::bail!("Invalid dst kvcache storage!")
-            };
-            let gpu_num_blocks = src.dim(0)?;
-            let cpu_num_blocks = dst.dim(0)?;
-
-            let src_ptr = src_storage
-                .as_cuda_slice::<half::bf16>()
-                .map_err(candle_core::Error::wrap)?
-                .device_ptr();
-            let dst_slice: &[half::bf16] =
-                dst_storage.as_slice().map_err(candle_core::Error::wrap)?;
-            let ptr = dst_slice.as_ptr() as *mut half::bf16;
-
-            for (src_block_number, dst_block_number) in block_mapping {
-                assert!(
-                    *src_block_number < gpu_num_blocks,
-                    "Invalid gpu block {} / {}",
-                    src_block_number,
-                    gpu_num_blocks
-                );
-                assert!(
-                    *dst_block_number < cpu_num_blocks,
-                    "Invalid cpu block {} / {}",
-                    dst_block_number,
-                    cpu_num_blocks
-                );
-
-                let src_offset: u64 = (src_block_number * block_size_elements).try_into().unwrap();
-                let dst_offset: usize =
-                    (dst_block_number * block_size_elements).try_into().unwrap();
-                let dst_slice = unsafe {
-                    slice::from_raw_parts_mut(ptr.wrapping_add(dst_offset), block_size_elements)
-                };
-
-                let src_slice = unsafe {
-                    let slice: CudaSlice<half::bf16> =
-                        src_dev.upgrade_device_ptr(src_ptr + src_offset, block_size_elements);
-                    std::mem::ManuallyDrop::new(slice)
-                };
-
-                unsafe {
-                    result::memcpy_dtoh_async(
-                        dst_slice,
-                        *src_slice.device_ptr(),
-                        *src_dev.cu_stream(),
-                    )
-                    .map_err(candle_core::Error::wrap)?;
-                }
+            (src, dst) => {
+                candle_core::bail!("Tensors must be on either the GPU or CPU to swap,, got {src:?} (src) and {dst:?} (dst).")
             }
-            let _ = src_dev.synchronize();
-        }
-        (src, dst) => {
-            candle_core::bail!("Tensors must be on either the GPU or CPU to swap,, got {src:?} (src) and {dst:?} (dst).")
         }
     }
 
-    Ok(())
+    match src.dtype() {
+        DType::F16 => cuda_fwd::<f16>(src, dst, block_mapping),
+        DType::BF16 => cuda_fwd::<bf16>(src, dst, block_mapping),
+        DType::U8 => cuda_fwd::<u8>(src, dst, block_mapping),
+        _ => {
+            candle_core::bail!("swap_blocks only accept f16/bf16/u8 kvcache dtypes!")
+        }
+    }
 }
 
 #[cfg(feature = "metal")]
