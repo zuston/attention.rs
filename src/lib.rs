d7cbc48aa2bdf213cc5b045320c5939199ef5457
Guoqing Bao <topon@outlook.com>
2025-11-19 11:23:57 +0000

    Refactor attention workflow

--- src/lib.rs
+++ src/lib.rs
@@ -82,18 +82,8 @@ impl PagedAttention {
         })
     }
 
-    #[cfg(not(feature = "flash-attn"))]
-    fn repeat_kv(&self, x: Tensor, n_rep: usize) -> Result<Tensor> {
-        if n_rep == 1 {
-            Ok(x)
-        } else {
-            let (b_sz, n_kv_head, seq_len, head_dim) = x.dims4()?;
-            Tensor::cat(&vec![&x; n_rep], 2)?.reshape((b_sz, n_kv_head * n_rep, seq_len, head_dim))
-        }
-    }
-
     #[allow(unused_variables)]
-    pub fn forward_prefill(
+    pub fn sdp_prefill(
         &self,
         query: &Tensor,
         key: &Tensor,
@@ -104,145 +94,196 @@ impl PagedAttention {
     ) -> Result<Tensor> {
         let (_, attention_heads, _, head_size) = query.shape().dims4()?;
         let (_, key_value_heads, _, _) = key.shape().dims4()?;
-
-        #[cfg(feature = "flash-attn")]
-        let att = {
-            let q = query
-                .transpose(1, 2)?
-                .reshape(((), attention_heads, head_size))?;
-            let key = if input_metadata.block_tables.is_none() {
-                let key = key
-                    .transpose(1, 2)?
-                    .reshape(((), key_value_heads, head_size))?;
-                key
+        fn repeat_kv(x: Tensor, n_rep: usize) -> Result<Tensor> {
+            if n_rep == 1 {
+                Ok(x)
             } else {
-                key.to_owned()
+                let (b_sz, n_kv_head, seq_len, head_dim) = x.dims4()?;
+                Tensor::cat(&vec![&x; n_rep], 2)?.reshape((
+                    b_sz,
+                    n_kv_head * n_rep,
+                    seq_len,
+                    head_dim,
+                ))
+            }
+        }
+        let indices = &input_metadata
+            .cu_seqlens_q
+            .as_ref()
+            .unwrap()
+            .to_vec1::<u32>()?[1..];
+        let seqlens: Vec<_> = indices.iter().map(|x| x).collect();
+
+        let mut vec_attn = Vec::new();
+        let mut start = 0usize;
+        //chunked attention for each sequence
+        for (i, seqlen) in seqlens.iter().enumerate() {
+            let seq_len = (**seqlen as usize - start) as usize;
+            let chunk_size = 1024;
+            let mut attn_chunks = vec![];
+
+            let query_seq = query.narrow(2, start, seq_len)?.contiguous()?;
+            let key_seq = key.narrow(2, start, seq_len)?.contiguous()?;
+            let value_seq = value.narrow(2, start, seq_len)?.contiguous()?;
+
+            let key_seq = if key_value_heads != attention_heads {
+                repeat_kv(key_seq, attention_heads / key_value_heads)?
+            } else {
+                key_seq
             };
 
-            let value = if input_metadata.block_tables.is_none() {
-                let value = value
-                    .transpose(1, 2)?
-                    .reshape(((), key_value_heads, head_size))?;
-                value
+            let value_seq = if key_value_heads != attention_heads {
+                repeat_kv(value_seq, attention_heads / key_value_heads)?
             } else {
-                value.to_owned()
+                value_seq
             };
 
-            let attn = if self.sliding_window.is_some() {
-                candle_flash_attn::flash_attn_varlen_windowed_softcap(
-                    &q,
-                    &key,
-                    &value,
-                    input_metadata.cu_seqlens_q.as_ref().unwrap(),
-                    input_metadata.cu_seqlens_k.as_ref().unwrap(),
-                    &input_metadata.block_tables,
-                    input_metadata.max_seqlen_q,
-                    input_metadata.max_seqlen_k,
-                    self.scale as f32,
-                    Some(softcapping.unwrap_or(0.0f64) as f32),
-                    self.sliding_window,
-                    Some(0),
-                )?
-            } else {
-                candle_flash_attn::flash_attn_varlen_softcap(
-                    &q,
-                    &key,
-                    &value,
-                    input_metadata.cu_seqlens_q.as_ref().unwrap(),
-                    input_metadata.cu_seqlens_k.as_ref().unwrap(),
-                    &input_metadata.block_tables,
-                    input_metadata.max_seqlen_q,
-                    input_metadata.max_seqlen_k,
-                    self.scale as f32,
-                    Some(softcapping.unwrap_or(0.0f64) as f32),
-                    true,
-                )?
-            };
-            attn
-        };
+            let num_chunks = (seq_len + chunk_size - 1) / chunk_size;
 
-        #[cfg(not(feature = "flash-attn"))]
-        let att = {
-            let indices = &input_metadata
-                .cu_seqlens_q
-                .as_ref()
-                .unwrap()
-                .to_vec1::<u32>()?[1..];
-            let seqlens: Vec<_> = indices.iter().map(|x| x).collect();
+            for c in 0..num_chunks {
+                let offset = c * chunk_size;
+                let len = chunk_size.min(seq_len - offset);
+                //chunk at query is correct for the following
+                let q_chunk = query_seq.narrow(2, offset, len)?.contiguous()?;
+                let mut att = (q_chunk.matmul(&key_seq.t()?)? * f64::from(self.scale))?;
 
-            let mut vec_attn = Vec::new();
-            let mut start = 0usize;
-            //chunked attention for each sequence
-            for (i, seqlen) in seqlens.iter().enumerate() {
-                let seq_len = (**seqlen as usize - start) as usize;
-                let chunk_size = 1024;
-                let mut attn_chunks = vec![];
-
-                let query_seq = query.narrow(2, start, seq_len)?.contiguous()?;
-                let key_seq = key.narrow(2, start, seq_len)?.contiguous()?;
-                let value_seq = value.narrow(2, start, seq_len)?.contiguous()?;
-
-                let key_seq = if key_value_heads != attention_heads {
-                    self.repeat_kv(key_seq, attention_heads / key_value_heads)?
-                } else {
-                    key_seq
-                };
-
-                let value_seq = if key_value_heads != attention_heads {
-                    self.repeat_kv(value_seq, attention_heads / key_value_heads)?
-                } else {
-                    value_seq
-                };
-
-                let num_chunks = (seq_len + chunk_size - 1) / chunk_size;
-
-                for c in 0..num_chunks {
-                    let offset = c * chunk_size;
-                    let len = chunk_size.min(seq_len - offset);
-                    //chunk at query is correct for the following
-                    let q_chunk = query_seq.narrow(2, offset, len)?.contiguous()?;
-                    let mut att = (q_chunk.matmul(&key_seq.t()?)? * f64::from(self.scale))?;
-
-                    if let Some(sc) = softcapping {
-                        att = ((att / sc)?.tanh()? * sc)?;
-                    }
-
-                    if let Some(mask) = &attention_mask {
-                        //mask needs to be chunked
-                        let q_chunk_mask = mask[i].narrow(2, offset, len)?; // shape: [1, 1, chunk_len, K_len]
-                        att = att.broadcast_add(&q_chunk_mask)?;
-                    }
-
-                    att =
-                        candle_nn::ops::softmax_last_dim(&att.to_dtype(candle_core::DType::F32)?)?
-                            .to_dtype(att.dtype())?;
-
-                    let att_chunk = att.matmul(&value_seq)?;
-                    attn_chunks.push(att_chunk);
+                if let Some(sc) = softcapping {
+                    att = ((att / sc)?.tanh()? * sc)?;
                 }
 
-                let att = Tensor::cat(&attn_chunks, 2)?.contiguous()?;
-                vec_attn.push(att);
+                if let Some(mask) = &attention_mask {
+                    //mask needs to be chunked
+                    let q_chunk_mask = mask[i].narrow(2, offset, len)?; // shape: [1, 1, chunk_len, K_len]
+                    att = att.broadcast_add(&q_chunk_mask)?;
+                }
 
-                start = **seqlen as usize;
+                att = candle_nn::ops::softmax_last_dim(&att.to_dtype(candle_core::DType::F32)?)?
+                    .to_dtype(att.dtype())?;
+
+                let att_chunk = att.matmul(&value_seq)?;
+                attn_chunks.push(att_chunk);
             }
-            Tensor::cat(&vec_attn, 2)?.contiguous()?.transpose(1, 2)?
-        };
 
-        Ok(att)
+            let att = Tensor::cat(&attn_chunks, 2)?.contiguous()?;
+            vec_attn.push(att);
+
+            start = **seqlen as usize;
+        }
+        Tensor::cat(&vec_attn, 2)?.contiguous()?.transpose(1, 2)
+    }
+
+    #[cfg(feature = "flash-attn")]
+    pub fn flash_var_len(
+        &self,
+        query: &Tensor,
+        key: &Tensor,
+        value: &Tensor,
+        input_metadata: &InputMetadata,
+        softcapping: Option<f64>,
+    ) -> Result<Tensor> {
+        if self.sliding_window.is_some() {
+            candle_flash_attn::flash_attn_varlen_windowed_softcap(
+                query,
+                key,
+                value,
+                input_metadata.cu_seqlens_q.as_ref().unwrap(),
+                input_metadata.cu_seqlens_k.as_ref().unwrap(),
+                &input_metadata.block_tables,
+                input_metadata.max_seqlen_q,
+                input_metadata.max_seqlen_k,
+                self.scale as f32,
+                Some(softcapping.unwrap_or(0.0f64) as f32),
+                self.sliding_window,
+                Some(0),
+            )
+        } else {
+            candle_flash_attn::flash_attn_varlen_softcap(
+                query,
+                key,
+                value,
+                input_metadata.cu_seqlens_q.as_ref().unwrap(),
+                input_metadata.cu_seqlens_k.as_ref().unwrap(),
+                &input_metadata.block_tables,
+                input_metadata.max_seqlen_q,
+                input_metadata.max_seqlen_k,
+                self.scale as f32,
+                Some(softcapping.unwrap_or(0.0f64) as f32),
+                true,
+            )
+        }
+    }
+
+    #[cfg(feature = "flash-attn")]
+    pub fn flash_forward(
+        &self,
+        query: &Tensor,
+        key: &Tensor,
+        value: &Tensor,
+        key_cache: Option<Tensor>,
+        value_cache: Option<Tensor>,
+        input_metadata: &InputMetadata,
+        softcapping: Option<f64>,
+    ) -> Result<Tensor> {
+        let (_, attention_heads, _, head_size) = query.shape().dims4()?;
+        let (_, key_value_heads, _, _) = key.shape().dims4()?;
+        let slot_mapping = input_metadata.slot_mapping.flatten_all()?;
+        let query = query
+            .transpose(1, 2)?
+            .reshape(((), attention_heads, head_size))?;
+        let key = key
+            .transpose(1, 2)?
+            .reshape(((), key_value_heads, head_size))?;
+        let value = value
+            .transpose(1, 2)?
+            .reshape(((), key_value_heads, head_size))?;
+
+        reshape_and_cache(
+            &key,
+            &value,
+            key_cache.as_ref().unwrap(),
+            value_cache.as_ref().unwrap(),
+            self.k_scale.as_ref(),
+            self.v_scale.as_ref(),
+            &slot_mapping,
+        )?;
+
+        if input_metadata.is_prefill {
+            return if input_metadata.block_tables.is_none() {
+                // prefill without kvcache
+                self.flash_var_len(&query, &key, &value, input_metadata, softcapping)
+            } else {
+                // prefill with kvcache
+                self.flash_var_len(
+                    &query,
+                    key_cache.as_ref().unwrap(),
+                    value_cache.as_ref().unwrap(),
+                    input_metadata,
+                    softcapping,
+                )
+            };
+        }
+
+        #[cfg(feature = "flash-decoding")]
+        {
+            let block_tables = input_metadata.block_tables.as_ref().unwrap();
+            let context_lens = input_metadata.context_lens.as_ref().unwrap();
+            candle_flash_attn::flash_attn_with_kvcache(
+                &query.unsqueeze(1)?, //(batch_size, seqlen_q, num_heads_q, head_size)
+                key_cache.as_ref().unwrap(),
+                value_cache.as_ref().unwrap(),
+                context_lens,
+                block_tables,
+                self.scale as f32,
+            )
+        }
+        #[cfg(not(feature = "flash-decoding"))]
+        candle_core::bail!("Invalid pattern for flash_forward")
     }
 
     #[allow(clippy::too_many_arguments)]
     #[allow(unused_variables)]
-    /// query: shape = [batch_size, seq_len, num_heads * head_size]
-    /// key: shape = [batch_size, seq_len, num_kv_heads * head_size]
-    /// value: shape = [batch_size, num_kv_heads * head_size]
-    /// key_cache: shape = [num_blocks, num_kv_heads, head_size/x,
-    ///     block_size, x]
-    /// value_cache: shape = [num_blocks, num_kv_heads, head_size,
-    ///     block_size]
-    /// input_metadata: metadata for paged attention.
     #[allow(unused_mut)]
+    #[allow(unreachable_code)]
     pub fn forward(
         &self,
         query: &Tensor,
@@ -261,19 +302,34 @@ impl PagedAttention {
             }
         }
 
-        let dims = input_metadata.slot_mapping.dims();
-        let slot_mapping = if dims.len() > 1 {
-            input_metadata.slot_mapping.flatten_all()?
-        } else {
-            input_metadata.slot_mapping.clone()
-        };
+        #[cfg(feature = "flash-decoding")]
+        return self.flash_forward(
+            query,
+            key,
+            value,
+            key_cache,
+            value_cache,
+            input_metadata,
+            softcapping,
+        );
 
-        let (batch_size, attention_heads, seq_len, head_size) = query.shape().dims4()?;
-        let (_, key_value_heads, _, _) = key.shape().dims4()?;
+        if input_metadata.is_prefill && input_metadata.block_tables.is_none() {
+            // non context-cache prefill with flash-attn
+            #[cfg(feature = "flash-attn")]
+            return self.flash_forward(
+                query,
+                key,
+                value,
+                key_cache,
+                value_cache,
+                input_metadata,
+                softcapping,
+            );
+        }
 
         let mut att = if input_metadata.is_prefill && input_metadata.block_tables.is_none() {
-            //prefill - no context cache
-            Some(self.forward_prefill(
+            //no context cache, prefill with naive scale-dot-product attention
+            Some(self.sdp_prefill(
                 query,
                 key,
                 value,
@@ -285,6 +341,13 @@ impl PagedAttention {
             None
         };
 
+        // The following for paged attention
+        let slot_mapping = input_metadata.slot_mapping.flatten_all()?;
+
+        let (batch_size, attention_heads, seq_len, head_size) = query.shape().dims4()?;
+        let (_, key_value_heads, _, _) = key.shape().dims4()?;
+
+        // Write KvCache for SDP + Paged Attention
         let key = key
             .transpose(1, 2)?
             .reshape(((), key_value_heads, head_size))?;
@@ -292,9 +355,6 @@ impl PagedAttention {
             .transpose(1, 2)?
             .reshape(((), key_value_heads, head_size))?;
 
-        // key: Tensor,              // [num_tokens, num_heads, head_size]
-        // value: Tensor,            // [num_tokens, num_heads, head_size]
-        // slot_mapping: Tensor,     // [num_tokens]
         if key_cache.as_ref().is_some_and(|_| value_cache.is_some()) {
             reshape_and_cache(
                 &key,
@@ -305,19 +365,6 @@ impl PagedAttention {
                 self.v_scale.as_ref(),
                 &slot_mapping,
             )?;
-
-            #[cfg(feature = "flash-decoding")]
-            if input_metadata.is_prefill && input_metadata.block_tables.is_some() {
-                //context cache prefill
-                att = Some(self.forward_prefill(
-                    query,
-                    key_cache.as_ref().unwrap(),
-                    value_cache.as_ref().unwrap(),
-                    attention_mask,
-                    input_metadata,
-                    softcapping,
-                )?);
-            }
         }
 
         if let Some(att) = att {
@@ -330,20 +377,6 @@ impl PagedAttention {
         let query = query
             .transpose(1, 2)?
             .reshape(((), attention_heads, head_size))?;
-
-        #[cfg(feature = "flash-decoding")]
-        if key_cache.as_ref().is_some_and(|_| value_cache.is_some()) {
-            //decoding with falsh-attn
-            //key_cache, value_cache: [num_blocks, block_size, num_heads, head_size]
-            return candle_flash_attn::flash_attn_with_kvcache(
-                &query.unsqueeze(1)?, //(batch_size, seqlen_q, num_heads_q, head_size)
-                key_cache.as_ref().unwrap(),
-                value_cache.as_ref().unwrap(),
-                context_lens,
-                block_tables,
-                self.scale as f32,
-            );
-        }
 
         //decoding with paged-attn
         let max_context_len = if self.sliding_window.is_some() {
@@ -363,20 +396,7 @@ impl PagedAttention {
         } else {
             None
         };
-        //  Args:
-        //  output: shape = [num_generation_tokens, num_heads, head_size]
-        //
-        //  query: shape = [num_generation_tokens, num_heads, head_size]
-        //
-        //  key_cache: shape = [num_blocks, num_kv_heads, head_size/x,
-        //      block_size, x]
-        //
-        //  value_cache: shape = [num_blocks, num_kv_heads, head_size,
-        //      block_size]
-        //
-        //  input_metadata: metadata for paged attention.
-        //
-        //  alibi_slopes: shape = [num_heads]
+
         paged_attention(
             &query,
             key_cache.as_ref().unwrap(),
